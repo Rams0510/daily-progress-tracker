a34367c455145fecfc1df620ead45b12fcc82871
T Ramya <tramya552@gmail.com>
2026-06-28 19:53:09 +0530

    Use Render environment variables1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { google } = require('googleapis');
-const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
